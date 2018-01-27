12a2e8a3ed1ccd0aae6d3ec0c5be7db4bc24d478
root <root@localhost.localdomain>
2018-01-27 02:11:46 -0800

    First working commit; now testing deployment

--- Vagrantfile
+++ Vagrantfile
@@ -12,7 +12,7 @@ Vagrant.configure("2") do |config|
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://vagrantcloud.com/search.
-  config.vm.box = "centos/7"
+  config.vm.box = "ubuntu/xenial64"
   config.vm.network :forwarded_port, guest: 80, host: 8080
   config.vm.provision :shell, path: "bootstrap.sh"
 
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,25 @@
 #! /bin/bash
 
-yum -y update
+sudo apt-get -y update
+# For convenience
+sudo apt-get -y install mlocate vim python-pip
+# Docker setup
+sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get -y update
+apt-get -y install docker-ce
+
+# Now do app files
+cp -R /vagrant/app ~/
+cd ~/app
+# Build container
+docker build -t hello .
+# Run container in background
+docker run -d -p 80:80 -v /vagrant:/vagrant hello
+
+#Log file is in /vagrant/app.log
+
 
 if ! [[ -L /var/www ]]; then
 	rm -rf /var/www/
